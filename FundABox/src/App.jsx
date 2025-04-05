0dd67024af0857fdd6213f2dbf9f0c6b4bc2b059
Edward Hawkson <ehawkson@exeter.edu>
2025-04-05 16:42:28 -0400

    Auth and Nav changes

--- FundABox/src/App.jsx
+++ FundABox/src/App.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import LoadingScreen from "./components/LoadingScreen"; // Import the Loading Screen
 import Auth from './components/auth'; // Import the Auth component
-
+import Home from './pages/Home';
+import Guide from './pages/Guide';
+import Feed from './pages/Feed';
+import Donate from './pages/Donate';
+import Card from './components/card'
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
---
+++ FundABox/src/components/card.jsx
@@ -0,0 +1,14 @@
+
+
+    function Card(){
+        return(
+            <div className="cart">
+                <img></img>
+                <h2>Feed</h2>
+                <p></p>
+            </div>
+        );
+
+    }
+
+export default Card
