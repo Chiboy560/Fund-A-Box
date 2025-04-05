62a2dcd2acc832cbff71ff77b7dc2c59d77f925e
Edward Hawkson <ehawkson@exeter.edu>
2025-04-05 17:08:02 -0400

    Seperate Pages

--- FundABox/src/App.jsx
+++ FundABox/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import LoadingScreen from "./components/LoadingScreen"; // Import the Loading Screen
 import Auth from './components/auth'; // Import the Auth component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Guide from './pages/Guide';
 import Feed from './pages/Feed';
@@ -29,7 +30,18 @@ const App = () => {
     return <Auth onLogin={handleLogin} />;
   }
 
-  return <Navbar />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>  {/* Use Routes here */}
+        <Route path="/" element={<Home />} />
+        <Route path="/guide" element={<Guide />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/donate" element={<Donate />} />
+      </Routes>
+    </Router>
+  );
+
 };
 
 export default App;
