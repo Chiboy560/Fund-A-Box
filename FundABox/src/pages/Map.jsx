03e0988945f76101ae3a197f52fcdad77837b56d
Edward Hawkson <ehawkson@exeter.edu>
2025-04-06 00:57:37 -0400

    Linked to google maps API

--- FundABox/src/pages/Map.jsx
+++ FundABox/src/pages/Map.jsx
@@ -1,67 +1,22 @@
 // components/Map.jsx
 import React, { useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader"
 
+//https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5BiUpn_KCk-olCVGnWretgcTE3HXDJA`
 const Map = () => {
   useEffect(() => {
-    const initMap = () => {
+    const loader = new Loader({
+      apiKey: "AIzaSyBj5BiUpn_KCk-olCVGnWretgcTE3HXDJA",
+      version: "weekly",
+      libraries: ["places"],
+    });
+
+    loader.load().then(() => {
       const map = new window.google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 6,
+        zoom: 8,
       });
-
-      const infoWindow = new window.google.maps.InfoWindow();
-      const locationButton = document.createElement("button");
-
-      locationButton.textContent = "Pan to Current Location";
-      locationButton.className =
-        "bg-white shadow px-3 py-2 rounded text-sm font-medium hover:bg-gray-100 m-2";
-
-      map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-
-      locationButton.addEventListener("click", () => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              };
-
-              infoWindow.setPosition(pos);
-              infoWindow.setContent("Location found.");
-              infoWindow.open(map);
-              map.setCenter(pos);
-            },
-            () => {
-              handleLocationError(true, infoWindow, map.getCenter());
-            }
-          );
-        } else {
-          // Browser doesn't support geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      });
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-          browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
-      }
-    };
-
-    if (!window.google || !window.google.maps) {
-      const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5BiUpn_KCk-olCVGnWretgcTE3HXDJA`;
-      script.defer = true;
-      window.initMap = initMap;
-      document.head.appendChild(script);
-    } else {
-      initMap();
-    }
+    });
   }, []);
 
   return (
