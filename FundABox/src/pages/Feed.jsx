80d6b0cca8fd0647b3628a2b7c74d4e24dfb5285
Edward Hawkson <ehawkson@exeter.edu>
2025-04-05 22:29:17 -0400

    Added some card functionality

--- FundABox/src/pages/Feed.jsx
+++ FundABox/src/pages/Feed.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Card from '../components/card' 
+import { db } from '../config/firebase'
+import { collection,  addDoc} from 'firebase/firestore'
 
 const Feed = () => {
     return (
         <div>
-            <h1>Feed</h1>
+            <div>
+                <h1>Feed</h1>
+            </div>
+            <div className="flex items-center justify-center min-h-screen bg-white">
+                <Card/>
+            </div>
         </div>
         
     )
